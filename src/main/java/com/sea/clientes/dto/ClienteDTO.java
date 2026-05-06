cd4a7113a131b0007911af026e4de50f9deb48ba
jvictor31 <joaovictorportella@hotmail.com>
2026-05-05 21:19:44 -0300

    feat: melhora validação do ClienteDTO incluindo validação de listas e ajuste de nome

--- src/main/java/com/sea/clientes/dto/ClienteDTO.java
+++ src/main/java/com/sea/clientes/dto/ClienteDTO.java
@@ -2,6 +2,7 @@ package com.sea.clientes.dto;
 
 import javax.validation.constraints.*;
 import lombok.Data;
+import javax.validation.Valid;
 import java.util.List;
 
 @Data
@@ -9,18 +10,22 @@ public class ClienteDTO {
 
     @NotBlank
     @Size(min = 3, max = 100)
-    @Pattern(regexp = "^[a-zA-ZÀ-ÿ0-9 ]+$")
+    @Pattern(regexp = "^[A-Za-zÀ-ú ]+$")
     private String nome;
 
     @NotBlank
+    @Pattern(regexp = "\\d{11}")
     private String cpf;
 
     @NotEmpty
+    @Valid
     private List<EnderecoDTO> enderecos;
 
     @NotEmpty
+    @Valid
     private List<TelefoneDTO> telefones;
 
     @NotEmpty
+    @Valid
     private List<EmailDTO> emails;
 }
